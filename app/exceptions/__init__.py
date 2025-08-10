624882d46a3dfdeb635225dc21aa04426af037ca
Hasan Maki <hasanmaki.ahmad@gmail.com>
2025-08-10 08:39:55 +0700

    feat: add exception imports to module

---
+++ app/exceptions/__init__.py
@@ -0,0 +1,11 @@
+from app.exceptions.exc_digipos import (
+    DigiposAuthenticationError,
+    DigiposMemberNotFoundError,
+    DigiposValidationError,
+)
+
+__all__ = [
+    "DigiposAuthenticationError",
+    "DigiposMemberNotFoundError",
+    "DigiposValidationError",
+]
