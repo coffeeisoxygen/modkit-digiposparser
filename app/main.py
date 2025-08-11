adc8e85a03348216aa3c798558c8fb9c28966098
Hasan Maki <hasanmaki.ahmad@gmail.com>
2025-08-11 20:05:14 +0700

    chore: add debug log level to uvicorn run command

--- app/main.py
+++ app/main.py
@@ -43,4 +43,6 @@ async def read_root():
 
 
 if __name__ == "__main__":
-    uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(
+        app="main:app", host="0.0.0.0", port=8000, reload=True, log_level="debug"
+    )
