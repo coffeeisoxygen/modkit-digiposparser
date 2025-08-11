d55ddcaec823322db4a12c9bbfed9d12b16eaa50
Hasan Maki <hasanmaki.ahmad@gmail.com>
2025-08-11 18:02:20 +0700

    refactor: move OtomaxSignatureService to utils

--- app/feature/member/srv_member_auth.py
+++ app/feature/member/srv_member_auth.py
@@ -7,7 +7,7 @@ from app.exceptions.custom.exc_member import (
     MemberNotFoundError,
 )
 from app.feature.member.sch_member import MemberInDB, MemberTrxRequestModel
-from app.feature.member.srv_signature import OtomaxSignatureService
+from app.utils.siganture import OtomaxSignatureService
 from loguru import logger
 
 
