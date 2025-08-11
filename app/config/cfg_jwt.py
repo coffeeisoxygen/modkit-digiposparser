0378b3be7dbe2161f19cf851bd05db73bc8485f7
Hasan Maki <hasanmaki.ahmad@gmail.com>
2025-08-11 18:38:01 +0700

    refactor: simplify JwtConfig field definitions
    removed default values and aliases from JwtConfig fields for clarity and to enforce explicit configuration

--- app/config/cfg_jwt.py
+++ app/config/cfg_jwt.py
@@ -1,12 +1,9 @@
 """configuration for JWT authentication"""
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 
 
 class JwtConfig(BaseModel):
-    SECRET_KEY: str = Field(
-        default="ini-bikin-diopen-ssl -rand hex 32-09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7",
-        alias="JWT_SECRET_KEY",
-    )
-    ALGORITHM: str = Field(default="HS256", alias="JWT_ALGORITHM")
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = Field(default=60, alias="JWT_MINUTES_EXPIRE")
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
